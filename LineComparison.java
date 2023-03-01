6c51469056b15418e48c98c8b74ceb4139cff022
gaurav459433 <gaurav459433@gmail.com>
2023-03-01 20:52:29 +0530

    Refactor: Code For Checking & Compair Lenght Of Lines Using Oops Concepts

--- LineComparison.java
+++ LineComparison.java
@@ -5,24 +5,28 @@ import java.util.Scanner;
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome To Line Comparison Program");
-        double x1, y1, x2, y2, x3, y3, x4, y4;
+       LineComparison lc = new LineComparison();
+       lc.calculateLenght();
+    }
 
+    public void calculateLenght() {
+        double x1, y1, x2, y2, x3, y3, x4, y4;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter The Value Of x1:");
-        x1=sc.nextDouble();
+        x1 = sc.nextDouble();
 
         System.out.println("Enter The Value Of y1:");
-        y1=sc.nextDouble();
+        y1 = sc.nextDouble();
 
         System.out.println("Enter The Value Of x2:");
-        x2=sc.nextDouble();
+        x2 = sc.nextDouble();
 
         System.out.println("Enter The Value Of y2:");
-        y2=sc.nextDouble();
+        y2 = sc.nextDouble();
 
-        double length1 = Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
-        System.out.println("Length Of Line Is :" + length1);
+        double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length Of Line1 Is :" + lengthOfLine1);
 
         System.out.println("Enter The Number x3 : ");
         x3 = sc.nextDouble();
@@ -32,22 +36,17 @@ public class LineComparison {
         x4 = sc.nextDouble();
         System.out.println("Enter The Number y4 : ");
         y4 = sc.nextDouble();
-        Double length2 = Math.sqrt((x4 - x3) * (x4 - x3)  +  (y4 - y3) * (y4 - y3));
+        Double lengthOfLine2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
 
-        System.out.println("Length of the second Line is : " + length2);
+        System.out.println("Length of Line2 is : " + lengthOfLine2);
         System.out.println();
 
-        if (length1 == length2)
-        {
+        if (lengthOfLine1 == lengthOfLine2) {
             System.out.println("Length Of Both Line Are Equal:");
-        }
-        else if (length1 > length2)
-        {
-            System.out.println("Length is Greater Then Other Line:");
-        }
-        else
-        {
-            System.out.println("Length is Less Then Other Line:");
+        } else if (lengthOfLine1 > lengthOfLine2) {
+            System.out.println("Length Of Line1 is Greater Then Line2:");
+        } else {
+            System.out.println("Length of Line1 is Less Then Line2:");
         }
     }
 }
